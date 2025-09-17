e880fcbdf592bac2ca89f3d15c7952a3fb54dcfd
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2025-09-17 15:58:54 -0300

    Criação dos fragments de acordo com o indiano

--- HelloWorld/app/src/main/java/com/example/helloworld/exercises/act1/models/Produto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.helloworld.exercises.act1.models
-
-data class Produto(var nome:String, var preco: Double, var cagoria: String) {
-    fun aplicarDesconto(porcentagem: Double) {
-        if (porcentagem in 0.0..100.0){
-            preco -= preco * (porcentagem /100)
-        }
-    }
-}

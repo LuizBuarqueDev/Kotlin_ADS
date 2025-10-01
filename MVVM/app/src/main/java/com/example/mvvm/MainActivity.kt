36c0d61e2092640a0768cd7dcaba2f476109eae5
Luiz_Buarque_Dev <143226652+LuizBuarqueDev@users.noreply.github.com>
2025-10-01 20:59:39 -0300

    Conclusão da atividade

---
+++ MVVM/app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.mvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mvvm.ui.theme.MVVMTheme
+import com.example.mvvm.view.TasksScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MVVMTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Chame sua tela aqui!
+                    TasksScreen()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MVVMTheme {
+        Greeting("Android")
+    }
+}

807ecc0db8d7f096ec052496ce481bb4545cb090
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2025-10-22 17:45:15 -0300

    Base do navegation

---
+++ BookStore/app/src/main/java/com/example/bookstore/ui/DetailsFragment.kt
@@ -0,0 +1,36 @@
+package com.example.bookstore.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.bookstore.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: DetailsFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bookTitle = args.bookTitle
+        binding.textViewBookTitle.text = "Detalhes do livro:\n\n$bookTitle"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
